545ab9008d4641f74923b3f8e38a106e5d493404
HybridNeos <19171735+HybridNeos@users.noreply.github.com>
2019-11-17 12:56:22 -0500

    Code to join the datasets

--- join.py
+++ join.py
@@ -6,5 +6,15 @@ income = pd.read_csv("./Data/2015_Median_Income_by_County.csv")
 rent = pd.read_csv("./Data/filtered_FY_2016F_50_RevFinal.csv")
 
 # %% add a dummy column for county, state to join on
+incomeJoin = pd.Series( [income.iloc[i]["County"] + ", "+income.iloc[i]["State Code"] for i in range(len(income))] )
+income = pd.concat((income, incomeJoin.rename("incomeJoin")), axis=1)
+print(income.head())
 
-# %% perform the join and remove dummy columns and duplicates??
+# %% perform the join and remove the dummy columns
+df = crime.merge(right=rent, how="left", left_on="county_name", right_on="areaname")
+df = df.merge(right=income, how="left", left_on="county_name", right_on="incomeJoin")
+df.drop(columns=["incomeJoin"], inplace=True)
+
+# %% save it
+df.sort_values(['state_alpha', 'countyname'], inplace=True)
+df.to_csv("./Data/raw_joined_county_data.csv", index=False)
