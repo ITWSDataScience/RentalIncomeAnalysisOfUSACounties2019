a189e5b1a9a2530356dc6cb1432d891bad6916c9
Mark Shapiro <mark3198@gmail.com>
2019-11-20 15:13:28 -0500

    Joining finished

--- join.py
+++ join.py
@@ -1,20 +1,23 @@
 # %%
 import pandas as pd
 
-crime = pd.read_csv("./Data/crime_data_w_population_and_crime_rate.csv")
-income = pd.read_csv("./Data/2015_Median_Income_by_County.csv")
-rent = pd.read_csv("./Data/filtered_FY_2016F_50_RevFinal.csv")
+crime = pd.read_csv("./Datasets/Original datasets/crime_data_w_population_and_crime_rate.csv")
+income = pd.read_csv("./Datasets/Original datasets/median_income.csv")
+rent = pd.read_csv("./Datasets/Synthesized datasets/filtered_FY_2016F_50_RevFinal.csv")
 
 # %% add a dummy column for county, state to join on
 incomeJoin = pd.Series( [income.iloc[i]["County"] + ", "+income.iloc[i]["State Code"] for i in range(len(income))] )
 income = pd.concat((income, incomeJoin.rename("incomeJoin")), axis=1)
+rentJoin = pd.Series( [rent.iloc[i]["countyname"] + ", "+rent.iloc[i]["state_alpha"] for i in range(len(rent))] )
+rent = pd.concat((rent, rentJoin.rename("rentJoin")), axis=1)
 
 # %% perform the join and remove the dummy columns
-df = crime.merge(right=rent, how="left", left_on="county_name", right_on="areaname")
+df = crime.merge(right=rent, how="left", left_on="county_name", right_on="rentJoin")
 df = df.merge(right=income, how="left", left_on="county_name", right_on="incomeJoin")
-df.drop(columns=["incomeJoin", "State_x"], inplace=True)
+df.drop(columns=["incomeJoin", "State_x", "rentJoin"], inplace=True)
 df.rename(columns={'State_y':"State"}, inplace=True)
 
-# %% save it
+# %% final sorting then save it
+df = df[df["Rent50_0"].notnull()] #LaSalle County, IL has no rent data
 df.sort_values(['state_alpha', 'countyname'], inplace=True)
-df.to_csv("./Data/raw_joined_county_data.csv", index=False)
+df.to_csv("./Datasets/Synthesized datasets/raw_joined_county_data.csv", index=False)
--- synthesizeCut.py
+++ synthesizeCut.py
@@ -1,6 +1,10 @@
+# Get the data and important columns
 import pandas as pd
-df = pd.read_csv("./Data/raw_joined_county_data.csv")
+df = pd.read_csv("./Datasets/Synthesized datasets/raw_joined_county_data.csv")
 df = df[["crime_rate_per_100000", "Rent50_0", "Median household income", "State Code", "State", "County"]]
+
+# Calculate yearly rent as ratio of median income
 ratio = pd.Series( [((df.iloc[i]["Rent50_0"]*12)/df.iloc[i]["Median household income"])*100 for i in range(len(df))] )
 df = pd.concat((df, ratio.rename("RentPercentOfIncome")), axis=1)
-df.to_csv("./Data/analysis_county_data.csv", index=False)
+df = df[df["Median household income"].notnull()]
+df.to_csv("./Datasets/Synthesized datasets/analysis_county_data.csv", index=False)
