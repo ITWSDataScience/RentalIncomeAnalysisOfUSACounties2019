6e8a2e69ac061ee745526b88ab72fdcc5a8ded65
Mark Shapiro <mark3198@gmail.com>
2019-11-21 16:53:30 -0500

    Fixed visualization and made customizable

--- testVisualization.py
+++ testVisualization.py
@@ -4,33 +4,35 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("./Datasets/Synthesized datasets/analysis_county_data.csv")
 
-rentSplits = 10
-incomeSplits = 10
+rentSize = 10
+rentSplits = np.linspace(min(df["Rent50_0"]), max(df["Rent50_0"])+1, rentSize)
+incomeSize = 10
+incomeSplits = np.linspace(min(df["Median household income"]), max(df["Median household income"])+1, incomeSize)
+incomeSplits = [round(x) for x in incomeSplits]
 
-hist, edges = np.histogram(df["Rent50_0"], rentSplits)
-hist2, edges2 = np.histogram(df["Median household income"], incomeSplits)
-edges = [round(x, 2) for x in edges]
-edges2 = [round(x, 2) for x in edges2]
+#hist, edges = np.histogram(df["Rent50_0"], rentSize)
+#hist2, edges2 = np.histogram(df["Median household income"], incomeSize)
+#edges = [round(x, 2) for x in edges]
 
-matrix = np.zeros((rentSplits, incomeSplits))
+matrix = np.zeros((rentSize-1, incomeSize-1))
 for i in range(len(df)):
     rent = df.iloc[i]["Rent50_0"]
     income = df.iloc[i]["Median household income"]
-    for j in range(1, rentSplits):
-        for k in range(1, incomeSplits):
-            if (rent >= edges[j-1] and rent < edges[j]) and (income >= edges2[k-1] and income < edges2[k]):
-                matrix[j, k] += 1
+    for j in range(1, rentSize+1):
+        for k in range(1, incomeSize+1):
+            if (rent >= rentSplits[j-1] and rent < rentSplits[j]) and (income >= incomeSplits[k-1] and income < incomeSplits[k]):
+                matrix[j-1, k-1] += 1
 
-matrix2 = np.zeros((rentSplits, incomeSplits))
+matrix2 = np.zeros((rentSize-1, incomeSize-1))
 for i in range(len(df)):
     rent = df.iloc[i]["Rent50_0"]
     income = df.iloc[i]["Median household income"]
-    for j in range(1, rentSplits):
-        for k in range(1, incomeSplits):
-            if (rent >= edges[j-1] and rent < edges[j]) and (income >= edges2[k-1] and income < edges2[k]):
-                matrix2[j, k] += df.iloc[i]["crime_rate_per_100000"]
+    for j in range(1, rentSize+1):
+        for k in range(1, incomeSize+1):
+            if (rent >= rentSplits[j-1] and rent < rentSplits[j]) and (income >= incomeSplits[k-1] and income < incomeSplits[k]):
+                matrix2[j-1, k-1] += df.iloc[i]["crime_rate_per_100000"]
 
-averageCrimeRate = np.empty((rentSplits, incomeSplits))
+averageCrimeRate = np.empty((rentSize-1, incomeSize-1))
 for i in range(matrix.shape[0]):
     for j in range(matrix.shape[1]):
         averageCrimeRate[i, j] = 0 if matrix[i, j] == 0 else round(matrix2[i, j]/matrix[i, j], 2)
@@ -39,8 +41,8 @@ for i in range(matrix.shape[0]):
 ms = plt.matshow(averageCrimeRate, cmap=plt.cm.Blues)
 plt.colorbar(ms, label="average crime per 100000 per section")
 plt.xlabel("Rent per county")
-plt.xticks(range(rentSplits), edges, rotation=90)
+plt.xticks(range(rentSize-1), rentSplits, rotation=90)
 plt.ylabel("Income")
-plt.yticks(range(incomeSplits), edges2)
+plt.yticks(range(incomeSize-1), incomeSplits)
 #plt.title("Crime psuedo heatmap")
 plt.show()
