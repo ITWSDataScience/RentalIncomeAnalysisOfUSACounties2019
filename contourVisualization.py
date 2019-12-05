4c0fa28a14a48e0089bb5dab6c9d0c91f8836485
Mark Shapiro <mark3198@gmail.com>
2019-12-05 13:17:38 -0500

    Contour visualization

--- contourVisualization.py
+++ contourVisualization.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 df = pd.read_csv("./Datasets/Synthesized datasets/analysis_county_data.csv")
 
@@ -9,10 +11,6 @@ rentSplits = np.linspace(min(df["Rent50_0"]), max(df["Rent50_0"])+1, rentSize)
 incomeSize = 10
 incomeSplits = np.linspace(min(df["Median household income"]), max(df["Median household income"])+1, incomeSize)
 incomeSplits = [round(x) for x in incomeSplits]
-
-#hist, edges = np.histogram(df["Rent50_0"], rentSize)
-#hist2, edges2 = np.histogram(df["Median household income"], incomeSize)
-#edges = [round(x, 2) for x in edges]
 
 matrix = np.zeros((rentSize-1, incomeSize-1))
 for i in range(len(df)):
@@ -37,12 +35,20 @@ for i in range(matrix.shape[0]):
     for j in range(matrix.shape[1]):
         averageCrimeRate[i, j] = 0 if matrix[i, j] == 0 else round(matrix2[i, j]/matrix[i, j], 2)
 
+# colormap
+coolwarm = cm.get_cmap('coolwarm', 256)
+newcolors = coolwarm(np.linspace(0, 1, 256))
+white = np.array([256/256, 256/256, 256/256, 1])
+newcolors[1:20, :] = white
+newcmp = ListedColormap(newcolors)
+
 # plot it
-ms = plt.matshow(averageCrimeRate, cmap=plt.cm.Blues)
+#ms = plt.matshow(averageCrimeRate, cmap=plt.cm.Blues)
+ms = plt.contourf(range(rentSize-1), range(incomeSize-1), averageCrimeRate, cmap=newcmp)
 plt.colorbar(ms, label="average crime per 100000 per section")
 plt.xlabel("Rent per county")
 plt.xticks(range(rentSize-1), rentSplits, rotation=90)
 plt.ylabel("Income")
 plt.yticks(range(incomeSize-1), incomeSplits)
-#plt.title("Crime psuedo heatmap")
+plt.title("Contour plot of Rent vs Income with Crime as color")
 plt.show()
